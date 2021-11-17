4ffb4841eb6acbafac3040f165260bd6c83d6615
yea <evansalimi53@gmail.com>
2021-11-17 22:11:15 +0700

    push pertama

---
+++ application/controllers/site.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class site extends CI_Controller {
+    function index(){
+
+     //   echo"tes";
+    $this->load->view("tutor1"); 
+    }
+}
